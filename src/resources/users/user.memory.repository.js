81f7a5df03632b757035f04023d6d0588a55edc8
gonzjv <gonzjv@ya.ru>
2021-12-01 10:33:07 +0300

    init: start from template & feat: users array

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,7 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
-
+const users = [
+  { id: '1', name: 'Max', login: 'trshkv' },
+  { id: '2', name: 'Sanya', login: 'agent007' },
+  { id: '3', name: 'Maryan', login: 'soviet-citizen' },
+];
+const getAll = async () => users;
 module.exports = { getAll };
