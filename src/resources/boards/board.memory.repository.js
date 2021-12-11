deb665dadf83107b59481508984403bf9a91eb2c
gonzjv <gonzjv@ya.ru>
2021-12-11 18:47:39 +0300

    fix: import modules extensions

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -1,29 +1,29 @@
-const { boards } = require('../in-memory-db.js');
-const Board = require('./board.model.js');
+// // const { boards } = require('../in-memory-db.js');
+// const Board = require('./board.model.js');
 
-const getAll = async () => boards;
+// const getAll = async () => boards;
 
-const getByID = async (id) => boards.find((board) => board.id === id);
+// const getByID = async (id) => boards.find((board) => board.id === id);
 
-const create = async (board) => {
-  boards.push(board);
-};
+// const create = async (board) => {
+//   boards.push(board);
+// };
 
-const update = async (oldId, board) => {
-  const oldBoard = await getByID(oldId);
-  if (oldBoard) {
-    boards[boards.indexOf(oldBoard)] = Board.toPut(oldId, board);
-  }
+// const update = async (oldId, board) => {
+//   const oldBoard = await getByID(oldId);
+//   if (oldBoard) {
+//     boards[boards.indexOf(oldBoard)] = Board.toPut(oldId, board);
+//   }
 
-  return getByID(oldId);
-};
+//   return getByID(oldId);
+// };
 
-const kick = async (id) => {
-  const elemToDelete = await getByID(id);
-  if (elemToDelete) {
-    const index = boards.indexOf(elemToDelete);
-    boards.splice(index, 1);
-  }
-};
+// const kick = async (id) => {
+//   const elemToDelete = await getByID(id);
+//   if (elemToDelete) {
+//     const index = boards.indexOf(elemToDelete);
+//     boards.splice(index, 1);
+//   }
+// };
 
-module.exports = { getAll, getByID, create, update, kick };
+// module.exports = { getAll, getByID, create, update, kick };
--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -1,31 +1,31 @@
-const { tasks } = require('../in-memory-db.js');
-const Task = require('./task.model.js');
+// const { tasks } = require('../in-memory-db.js');
+// const Task = require('./task.model.js');
 
-const getAll = async (boardId) =>
-  tasks.filter((task) => task.boardId === boardId);
+// const getAll = async (boardId) =>
+//   tasks.filter((task) => task.boardId === boardId);
 
-const getByID = async (boardId, id) =>
-  tasks.find((task) => task.boardId === boardId && task.id === id);
+// const getByID = async (boardId, id) =>
+//   tasks.find((task) => task.boardId === boardId && task.id === id);
 
-const create = async (task) => {
-  tasks.push(task);
-};
+// const create = async (task) => {
+//   tasks.push(task);
+// };
 
-const update = async (oldId, task) => {
-  const oldTask = await getByID(oldId);
-  if (oldTask) {
-    tasks[tasks.indexOf(oldTask)] = Task.toPut(oldId, task);
-  }
+// const update = async (oldId, task) => {
+//   const oldTask = await getByID(oldId);
+//   if (oldTask) {
+//     tasks[tasks.indexOf(oldTask)] = Task.toPut(oldId, task);
+//   }
 
-  return getByID(oldId);
-};
+//   return getByID(oldId);
+// };
 
-const kick = async (id) => {
-  const elemToDelete = await getByID(id);
-  if (elemToDelete) {
-    const index = tasks.indexOf(elemToDelete);
-    tasks.splice(index, 1);
-  }
-};
+// const kick = async (id) => {
+//   const elemToDelete = await getByID(id);
+//   if (elemToDelete) {
+//     const index = tasks.indexOf(elemToDelete);
+//     tasks.splice(index, 1);
+//   }
+// };
 
-module.exports = { getAll, getByID, create, update, kick };
+// module.exports = { getAll, getByID, create, update, kick };
