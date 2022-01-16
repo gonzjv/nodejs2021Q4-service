94012761e9770eb8dfd25edfbfb017f0085ae9ad
gonzjv <gonzjv@ya.ru>
2022-01-16 19:55:12 +0300

    docs: config comments

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,7 +1,19 @@
+// import {
+//   PORT_POSTGRES_HOST,
+//   // PORT_POSTGRES_CONTAINER,
+//   POSTGRES_USER,
+//   POSTGRES_PASSWORD,
+//   POSTGRES_DB,
+// } from './src/common/config';
+
 export default {
   type: 'postgres',
+  // host: PORT_POSTGRES_HOST,
   host: 'postgres',
   port: 5432,
+  // username: POSTGRES_USER,
+  // password: POSTGRES_PASSWORD,
+  // database: POSTGRES_DB,
   username: 'test',
   password: 'test',
   database: 'test',
